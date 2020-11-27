d83a5a9658254b9233b049bc6b7ed517450e6e09
koji509 <thislifek@yahoo.co.jp>
2020-11-27 21:35:04 +0900

    ユーザー管理機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'prototypes/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :prototypes, only: :index
+  devise_for :users
   root to: "prototypes#index"
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
+  end
+  resources :users, only: :show
 end
